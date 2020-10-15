00e15bff4d6e8fddeba2bc109a4c8fe9af4fecde
jesmin123 <jesminjames23@gmail.com>
2020-10-15 17:55:29 +0530

    Finished job UI design

--- lib/ui/tab_views/pause_request_tab.dart
+++ lib/ui/tab_views/pause_request_tab.dart
@@ -1,7 +1,12 @@
+
+
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
-import '../../constants/app_font_style.dart';
-import '../../constants/colors.dart';
-import '../../constants/strings.dart';
+import 'package:job_karo_floor_manager/constants/app_font_style.dart';
+import 'package:job_karo_floor_manager/constants/colors.dart';
+import 'package:job_karo_floor_manager/constants/dimen.dart';
+import 'package:job_karo_floor_manager/ui/widget/pause_request_item.dart';
+import 'package:line_awesome_icons/line_awesome_icons.dart';
 
 class PauseRequestTab extends StatefulWidget {
   @override
@@ -11,6 +16,16 @@ class PauseRequestTab extends StatefulWidget {
 class _PauseRequestTabState extends State<PauseRequestTab> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text(PAUSE_REQUEST,style: AppFontStyle.headingTextStyle(PRIMARY_COLOR),));
+    return ListView.separated(shrinkWrap: true,
+        scrollDirection: Axis.vertical,
+        itemBuilder: (context,position){
+          return PauseRequestItem();
+        },
+        separatorBuilder: (context,position){
+          return Divider(color: APP_BLACK_COLOR, thickness: THICKNESS,);
+        },
+        itemCount: 6
+    );
+
   }
 }
